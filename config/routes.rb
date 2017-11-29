d949ac87466f92d84462afda6d9869500163d40f
Dan Actor <danltactor@gmail.com>
2017-11-29 15:16:22 -0700

    push to git

---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/new'
+
+  get 'artists/edit'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/new'
+
+  get 'songs/edit'
+
+  get 'boards/index'
+
+  get 'boards/show'
+
+  get 'boards/new'
+
+  get 'boards/edit'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
